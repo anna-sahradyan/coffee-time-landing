c22593759b73d2f1afd36982cc4b8d817de3d75a
anna-sahradyan <a.sahradyan72@gmail.com>
2025-08-15 22:38:42 +0400

    first commit

---
+++ index.js
@@ -0,0 +1,67 @@
+const sliderData = [
+    {id: 1, img: '/images/bg_2.jpg'},
+    {id: 2, img: '/images/bg_1.jpg'},
+    {id: 3, img: '/images/bg_3.jpg'},
+];
+
+// Рендер оригинальных слайдов
+const slider = document.querySelector('.slider');
+slider.innerHTML = '';
+
+sliderData.forEach(item => {
+    const card = document.createElement('article');
+    card.className = 'card';
+    const img = document.createElement('img');
+    img.src = item.img;
+    img.alt = `Слайд ${item.id}`;
+    card.appendChild(img);
+    slider.appendChild(card);
+});
+
+const prevBtn = document.querySelector('.btn_prev')
+const prevNext = document.querySelector('.btn_next')
+prevBtn.addEventListener('click', () => {
+    slider.scrollBy({
+        left: -slider.clientWidth,
+        behavior: 'smooth'
+    })
+})
+prevNext.addEventListener('click', () => {
+    slider.scrollBy({
+        left: slider.clientWidth,
+        behavior: 'smooth'
+    })
+})
+document.addEventListener("DOMContentLoaded", () => {
+    const burger = document.querySelector(".burger");
+    const nav = document.querySelector(".nav");
+
+    burger.addEventListener("click", () => {
+        nav.classList.toggle("open");
+        const expanded = burger.getAttribute("aria-expanded") === "true";
+        burger.setAttribute("aria-expanded", !expanded);
+    });
+});
+
+
+/*hero*/
+const coffeeOffers = [
+    {id: 1, img: '/icons/cup_2.svg'},
+    {id: 2, img: '/icons/cup_2.svg'},
+    {id: 3, img: '/icons/cup_2.svg'},
+    {id: 4, img: '/icons/cup_2.svg'},
+
+
+]
+const coffeeOffer = document.querySelector('.coffee-offers');
+coffeeOffer.innerHTML = '';
+coffeeOffers.forEach((item) => {
+    const cardItem = document.createElement('article')
+    cardItem.className = 'card_offers';
+    const img = document.createElement('img');
+    img.src = item.img
+    img.className='card_img'
+    img.alt = `offers ${item.id}`;
+    cardItem.appendChild(img);
+    coffeeOffer.appendChild(cardItem)
+})
