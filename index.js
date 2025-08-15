b63ddab3992ba6cfcde2450b03913418c5cf756a
anna-sahradyan <a.sahradyan72@gmail.com>
2025-08-15 23:12:45 +0400

    updated

--- index.js
+++ index.js
@@ -65,3 +65,21 @@ coffeeOffers.forEach((item) => {
     cardItem.appendChild(img);
     coffeeOffer.appendChild(cardItem)
 })
+
+document.addEventListener("DOMContentLoaded", () => {
+    const burger = document.querySelector(".burger");
+    const nav = document.querySelector(".nav");
+    const navClose = document.querySelector(".nav-close");
+
+    burger.addEventListener("click", () => {
+        nav.classList.add("open");
+        burger.style.display = "none";
+        burger.setAttribute("aria-expanded", "true");
+    });
+
+    navClose.addEventListener("click", () => {
+        nav.classList.remove("open");
+        burger.style.display = "block";
+        burger.setAttribute("aria-expanded", "false");
+    });
+});
